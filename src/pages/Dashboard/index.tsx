90d68f8697c564fd11536dd7d4bf72f8b67dcd0e
jennifertakagi <jennitakagi@gmail.com>
2021-01-17 16:37:51 -0300

    feat: add schedule on Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -29,6 +38,32 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Schedule</h1>
+          <p>
+            <span>Today</span>
+            <span>Day 06</span>
+            <span>Monday</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Next appointment</strong>
+            <div>
+              <img src="/avatar_url" alt="User's name" />
+
+              <strong>User's name</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+
+        <Calendar />
+      </Content>
     </Container>
   );
 };
